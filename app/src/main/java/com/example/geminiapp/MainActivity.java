d45235e5366cf64ac0b682cfb52fac0323b4b352
creeeper3610 <kmsmysk.sorato@gmail.com>
2026-08-14 11:16:18 +0900

    MainActivity.java の更新

--- app/src/main/java/com/example/geminiapp/MainActivity.java
+++ app/src/main/java/com/example/geminiapp/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity {
                 }
             });
 
-            mWebView.loadUrl("https://google.com");
+            mWebView.loadUrl("https://gemini.google.com");
         }
 
         // 現在の画面にWebViewを貼り付ける
